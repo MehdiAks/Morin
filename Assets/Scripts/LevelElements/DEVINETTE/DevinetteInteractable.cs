40c9e38679cf87de484d5feddb43d5c4f5a7bd7d
Afanpeak <62901138+MehdiAks@users.noreply.github.com>
2026-05-21 14:57:26 +0200

    Disable devinette objects after correct interactions

--- Assets/Scripts/LevelElements/DEVINETTE/DevinetteInteractable.cs
+++ Assets/Scripts/LevelElements/DEVINETTE/DevinetteInteractable.cs
@@ -3,6 +3,68 @@ using UnityEngine;
 public class DevinetteInteractable : MonoBehaviour
 {
     [SerializeField] private int orderIndex = 0;
+    [SerializeField] private Collider[] collidersToDisable;
+
+    private bool isDisabled;
 
     public int OrderIndex => orderIndex;
+    public bool IsDisabled => isDisabled;
+
+    public void DisableInteraction()
+    {
+        if (isDisabled)
+        {
+            return;
+        }
+
+        isDisabled = true;
+
+        if (collidersToDisable != null && collidersToDisable.Length > 0)
+        {
+            foreach (Collider objectCollider in collidersToDisable)
+            {
+                if (objectCollider != null)
+                {
+                    objectCollider.enabled = false;
+                }
+            }
+
+            return;
+        }
+
+        Collider fallbackCollider = GetComponentInChildren<Collider>();
+        if (fallbackCollider != null)
+        {
+            fallbackCollider.enabled = false;
+        }
+    }
+
+    public void EnableInteraction()
+    {
+        if (!isDisabled)
+        {
+            return;
+        }
+
+        isDisabled = false;
+
+        if (collidersToDisable != null && collidersToDisable.Length > 0)
+        {
+            foreach (Collider objectCollider in collidersToDisable)
+            {
+                if (objectCollider != null)
+                {
+                    objectCollider.enabled = true;
+                }
+            }
+
+            return;
+        }
+
+        Collider fallbackCollider = GetComponentInChildren<Collider>();
+        if (fallbackCollider != null)
+        {
+            fallbackCollider.enabled = true;
+        }
+    }
 }
