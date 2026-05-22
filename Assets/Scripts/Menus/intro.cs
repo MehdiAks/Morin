c3ca3bff53b22388f376f14fd39c848cf8ea59b9
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-05-22 09:43:24 +0200

    lettre et intro

--- Assets/Scripts/Menus/intro.cs
+++ Assets/Scripts/Menus/intro.cs
@@ -1,21 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class intro : MonoBehaviour
 {
-	[SerializeField] private List<GameObject> texts = new List<GameObject>();
-	[SerializeField] private List<GameObject> images = new List<GameObject>();
+	[SerializeField] private List<GameObject> frames = new List<GameObject>();
+	[SerializeField] private GameObject btn_suivant;
+    [SerializeField] private GameObject btn_lancement;
+    public int indice = 0;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        frames[indice].SetActive(true);
+        btn_suivant.SetActive(true);
+        btn_lancement.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        if (Input.GetKeyDown(KeyCode.Space)){
+            suivant();
+        }
+    }
+
+    public void lancer(){
+        SceneManager.LoadScene("ACCEUIL");
+    }
+
+    public void suivant(){
+        frames[indice].SetActive(false);
+        indice++;
+        if (indice < frames.Count){
+            frames[indice].SetActive(true);
+        } else {
+            btn_suivant.SetActive(false);
+            btn_lancement.SetActive(true);
+        }
     }
 }
